e9e4e158b6d35075146e8d77d0d5c3a902a62fb8
Atul Pal <113812093+atulpal-ai@users.noreply.github.com>
2026-08-24 00:06:59 +0530

    Time: 147 ms (7.09%), Space: 113.6 MB (7.72%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,22 +1,20 @@
-#include <vector>
-#include <unordered_set>
-using namespace std;
-
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        
-        unordered_set<int> s;
-        
-        for(int num : nums){
-            
-            if(s.count(num)){
-                return true;
-            }
-            
-            s.insert(num);
+        set<int> s;
+
+for(int i = 0; i < nums.size(); i++)
+{
+    if(s.find(nums[i]) != s.end())
+    {
+        return true;
+    }
+
+    s.insert(nums[i]);
+}
+
+return false;
+
         }
         
-        return false;
-    }
 };
